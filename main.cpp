c2f52f90db74f45b6fc014ac11cca2d426029ba7
sommeonne <brukaliuk.serhii@lll.kpi.ua>
2024-10-04 18:08:52 +0300

    Complete FuncA implementation, describe class  and add comments for parameters

--- main.cpp
+++ main.cpp
@@ -3,8 +3,15 @@
 
 int main() {
     Calculator calc;
-    double result = calc.FuncA();
-    std::cout << "Result of FuncA: " << result << std::endl;
+    int n;
+    double x;
+    std::cout << "Enter the number of elements to sum: ";
+    std::cin >> n;
+    std::cout << "Enter the value of x: ";
+    std::cin >> x;
+    
+    double result = calc.FuncA(n, x);
+    std::cout << "Result of FuncA for n = " << n << " and x = " << x << ": " << result << std::endl;
     return 0;
 }
 
